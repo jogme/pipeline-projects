3300377f76e93dc411172eeb304a25addb129b69
Norbert Pocs <norbertpocs0@gmail.com>
2024-05-31 15:19:07 +0200

    Working basic pipeline
    Signed-off-by: Norbert Pocs <norbertpocs0@gmail.com>

--- bkk/bkk_gtfs.py
+++ bkk/bkk_gtfs.py
@@ -40,11 +40,12 @@ col_alerts_arch = 'alert_archive'
 #col_routes = 'routes'
 
 try:
-    pass #client = MongoClient(server)
+    client = MongoClient(server)
 except:
     print('Couldn\'t estabilish connection with MongoDB server.')
+    exit(-1)
 
-#db = client[db_name]
+db = client[db_name]
 
 # %%
 # create docs to include to the db
@@ -53,18 +54,30 @@ except:
 #                     'current_status':0, 'top_speed':0}],
 #            'vehicle_label':0}
 
+
+# there are routes which are NO_SERVICE; they come in the alert feed
+# they need to be filtered out, as they are ghosts in feed_tru
+no_service_routes = {}
+# effect 1 is NO_SERVICE
+for x in [x for x in feed_ale.entity if x.alert.effect == 1]:
+    r_id = list(set([ie.route_id for ie in x.alert.informed_entity]))
+    no_service_routes.update({r:[x.alert.active_period[0].start, x.alert.active_period[0].end] for r in r_id})
+
+#filter out trip_update feed from cancelled and not yet departed routes
+tru_filtered = [x for x in feed_tru.entity \
+    if not x.trip_update.trip.route_id in no_service_routes.keys() and \
+       x.trip_update.trip.schedule_relationship != 3 and \
+       x.trip_update.stop_time_update[0].arrival.time <= feed_tru.header.timestamp]
+
 #contains all the data which is going to the db
-# TODO
-# - filter what is already in the db and needs only an update
-# - what are the things that may change and need an update?
 vhc_array = []
-for x in feed_tru.entity:
+for x in tru_filtered:
     if x.trip_update.vehicle.id == '':
         continue
     tu = x.trip_update
     tut = tu.trip
     data = {'meta':{'date':tut.start_date, 'trip_id':tut.trip_id, 'route_id':tut.route_id, \
-                    'vehicle_id':tu.vehicle.id},
+                    'vehicle_id':tu.vehicle.id, 'id': x.id},
             'next_stop_index':0, 'stop_num':len(tu.stop_time_update), 'alive':True,
             'stops':[]}
     timestamp = tu.timestamp
@@ -79,16 +92,16 @@ for x in feed_tru.entity:
         if s.departure.time <= timestamp:
             stop_dic['departure_timestamp'] = s.departure.time
         data['stops'].append(stop_dic)
+    if data['next_stop_index'] == data['stop_num']:
+        data['alive'] = False
     vhc_array.append(data)
 
 #print(vhc_array)
 
 # get the trip_id-date pairs from the db and filter what's only needed to be updated
 # FIXME are trip_ids unique to a day?
-#trip_id_curs = db[col_vehicle].find({'meta.date':date.today().strftime('%Y%m%d')},
-#                                    {'meta.trip_id':1, '_id':0})
-#todays_trips = [x['meta']['trip_id'] for x in trip_id_curs]
-todays_trips = []
+trip_id_curs = db[col_vehicle].find({'meta.date':date.today().strftime('%Y%m%d')})
+todays_trips = [x['meta']['trip_id'] for x in trip_id_curs]
 
 vhc_new = []
 
@@ -99,14 +112,26 @@ else:
         if not x['meta']['trip_id'] in todays_trips:
             vhc_new.append(x)
             continue
-        # TODO just update
-        if x['alive']:
-            if x['next_stop_index'] == x['stop_num']:
-                x['alive'] = False
-                continue
-            for i in range(x['next_stop_index'], x['stop_num']):
+        db_id_filter = {'meta.id':x['meta']['id']}
+        db_alive = db[col_vehicle].find_one(db_id_filter, {'alive':1, '_id':0})
+        if db_alive:
+            db[col_vehicle].update_one(db_id_filter, \
+                                       {'$set':{'next_stop_index':x['next_stop_index']}})
+            db_next_stop = db[col_vehicle].find_one(db_id_filter, {'next_stop_index':1, '_id':0})['next_stop_index']
+            for i in range(db_next_stop, x['next_stop_index']):
                 stop = x['stops'][i]
-                #if stop['arrival_timestamp'] <=
+                if stop['arrival_timestamp'] and stop['departure_timestamp'] == 0:
+                    break
+                if stop['arrival_timestamp'] != 0:
+                    db[col_vehicle].update_one(db_id_filter, \
+                        {'$set':{'stops.'+str(i)+'.arrival_timestamp':stop['arrival_timestamp']}})
+                if stop['departure_timestamp'] != 0:
+                    db[col_vehicle].update_one(db_id_filter, \
+                        {'$set':{'stops.'+str(i)+'.departure_timestamp':stop['departure_timestamp']}})
+            if x['alive'] == False:
+                db[col_vehicle].update_one(db_id_filter, {'$set':{'alive':False}})
 
-#db[col_vehicle].insertMany(vhc_new)
-#client.close()
+db[col_vehicle].insert_many(vhc_new)
+client.close()
+
+print('Successfully added ', len(vhc_new), ' and updated ', len(vhc_array)-len(vhc_new),' items')
