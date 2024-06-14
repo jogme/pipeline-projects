5a836296872d5451ed51e40b6355f8dd24b53dd3
Norbert Pocs <norbertpocs0@gmail.com>
2024-06-14 12:06:41 +0200

    Make it work
    Signed-off-by: Norbert Pocs <norbertpocs0@gmail.com>

---
+++ bkk/dags/bkk_alerts.py
@@ -0,0 +1,124 @@
+import pendulum
+from datetime import timedelta, date, datetime
+import time
+
+import requests
+from google.transit import gtfs_realtime_pb2
+from pymongo import MongoClient
+from pymongo.errors import BulkWriteError
+
+from airflow.decorators import dag, task
+from airflow import AirflowException
+
+key = "35e94de6-6448-4192-897c-149ed04f4f9a"
+base_link = "https://go.bkk.hu/api/query/v1/ws/gtfs-rt/full"
+server = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000"
+db_name = 'bkk_data'
+col_alerts_cur = 'alert_active'
+col_alerts_arch = 'alert_archive'
+
+@dag(
+    "bkk_alerts",
+    description="Budapests public transport alerts data",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 1, 1),
+    tags=['bkk', 'alerts'],
+    schedule_interval = timedelta(minutes=30),
+    default_args={'retries':0}
+)
+def bkk_alerts():
+    """
+    ### Pipeline for BKK alerts
+    This loads the API of https://go.bkk.hu/api/query/v1/ws/gtfs-rt/full/Alerts.pb
+    and loads it in a MongoDB database
+    """
+    @task()
+    def transform():
+        """
+        save as:
+        d = {'bkk_id':0, 'time_start':0, 'time_end':0, 'cause':0, 'effect':0, 'header_text':0,
+             'descriptive_text:0, 'informed_entities':[{'route_id':0, 'stop_id':)}]}
+        """
+        url_alerts = "{}/Alerts.pb?key={}".format(base_link, key)
+
+        feed_ale = gtfs_realtime_pb2.FeedMessage()
+
+        r_ale = requests.get(url_alerts)
+        if r_ale.status_code != 200:
+            raise AirflowException('could not get alerts data')
+
+        feed_ale.ParseFromString(r_ale.content)
+
+        output = []
+        for x in feed_ale.entity:
+            a = x.alert
+            output.append({'bkk_id':x.id, 'time_start':a.active_period[0].start,
+                           'time_end':a.active_period[0].end, 'cause':a.cause, 'effect':a.effect,
+                           'header_text':a.header_text.translation[0].text,
+                           'descriptive_text':a.description_text.translation[0].text,
+                           'ttl_date': datetime.fromtimestamp(a.active_period[0].end),
+                           'informed_entities':
+                           [{'route_id':i.route_id, 'stop_id':i.stop_id} for i in a.informed_entity]})
+        return output
+
+    @task()
+    def remove_old(processed_input):
+        try:
+            client = MongoClient(server)
+            client.server_info()
+        except:
+            raise AirflowException('Couldn\'t estabilish connection with MongoDB server.')
+
+        db = client[db_name]
+        cur_bkk_ids = tuple([x['bkk_id'] for x in processed_input])
+        alert_to_del = [x['bkk_id'] for x in db[col_alerts_cur].find({'time_end':{'$eq':0}}, {'bkk_id':1}) if not x['bkk_id'] in cur_bkk_ids]
+        if len(alert_to_del) != 0:
+            db[col_alerts_cur].delete_many({'bkk_id':{'$in':alert_to_del}})
+
+        client.close()
+
+    @task()
+    def load(processed_input):
+        try:
+            client = MongoClient(server)
+            client.server_info()
+        except:
+            raise AirflowException('Couldn\'t estabilish connection with MongoDB server.')
+
+        db = client[db_name]
+        # get the still active alerts from the db
+        cur_alerts = tuple([x['bkk_id'] for x in db[col_alerts_cur].find({}, {'bkk_id':1})])
+        if len(cur_alerts) == 0:
+            data_to_insert = processed_input
+        else:
+            data_to_insert = [x for x in processed_input if not x['bkk_id'] in cur_alerts]
+        ttl_index = {x['bkk_id']:{'expireAfterSeconds'
+                     :x['time_end'] - int(time.mktime(datetime.now().timetuple()))}
+                    for x in data_to_insert}
+
+        if len(data_to_insert) > 0:
+            """
+            # These indexes are needed to be specified on the "alert_active" and "alert_archive"
+            # collections to work properly. The indexes are needed to be defined only once, therefore
+            # excluding them from the script.
+            # to kill the old alerts
+            db[col_alerts_cur].create_index(['ttl_date'], name='timout death',
+                                            expireAfterSeconds=20, background=True,
+                                            partialFilterExpression={'time_end':{'$gt':0}})
+            # this index prevents duplicate items in the archive
+            db[col_alerts_arch].create_index(['bkk_id'], unique=True)
+            """
+            db[col_alerts_cur].insert_many(data_to_insert)
+            try:
+                db[col_alerts_arch].insert_many(data_to_insert, ordered=False)
+            except BulkWriteError:
+                # this error is raised when non-unique value is tried to be inserted too
+                # let's just ignore it
+                pass
+        client.close()
+    
+    processed_data = transform()
+    remove_old(processed_data)
+    load(processed_data)
+
+bkk_alerts()
---
+++ bkk/dags/bkk_trip_update.py
@@ -0,0 +1,185 @@
+import pendulum
+from datetime import timedelta, date
+
+import requests
+from google.transit import gtfs_realtime_pb2
+from pymongo import MongoClient
+
+from airflow.decorators import dag, task
+from airflow import AirflowException
+
+key = "35e94de6-6448-4192-897c-149ed04f4f9a"
+base_link = "https://go.bkk.hu/api/query/v1/ws/gtfs-rt/full"
+
+@dag(
+    "bkk_trip_updates",
+    description="Budapests public transport trip data",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 1, 1),
+    tags=['bkk', 'trip updates'],
+    schedule_interval = timedelta(seconds=10),
+    default_args={'retries':0}
+)
+def bkk_trip():
+    """
+    ### Pipeline for BKK trip update
+    This loads the API of https://go.bkk.hu/api/query/v1/ws/gtfs-rt/full/TripUpdates.pb
+    and loads it in a MongoDB database
+    """
+    @task()
+    def extract():
+        url_trip_upd = "{}/TripUpdates.pb?key={}".format(base_link, key)
+
+        feed_tru = gtfs_realtime_pb2.FeedMessage()
+
+        r_tru = requests.get(url_trip_upd)
+        if r_tru.status_code != 200:
+            raise AirflowException('could not get alerts data')
+
+        feed_tru.ParseFromString(r_tru.content)
+        return feed_tru
+
+    @task()
+    def get_current_alerts():
+        # fetch from source for now
+        # TODO fetch from db and ensure it gets pulled first
+        #url_alerts = "{}/Alerts.pb?key={}".format(base_link, key)
+        #feed_ale = gtfs_realtime_pb2.FeedMessage()
+
+        #r_ale = requests.get(url_alerts)
+
+        #if r_ale.status_code != 200:
+        #    raise AirflowException('could not get alerts data')
+        #return feed_ale.ParseFromString(r_ale.content)
+        # this returns alerts with NO_SERVICE effect
+        server = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000"
+        db_name = 'bkk_data'
+        col_alerts_cur = 'alert_active'
+
+        try:
+            client = MongoClient(server)
+            # force connection to see if the server is running
+            client.server_info()
+        except:
+            raise AirflowException('Couldn\'t estabilish connection with MongoDB server.')
+
+        db = client[db_name]
+        # get the still active alerts from the db
+        entities = db[col_alerts_cur].find({}, {'informed_entities':1})
+        #out = tuple([x.route_id for x in inf_ent])
+        out = tuple([i['route_id'] for x in entities for i in x])
+        client.close()
+        return out
+
+    @task()
+    def transform(input, alerts):
+        # there are routes which are NO_SERVICE; they come in the alert feed
+        # they need to be filtered out, as they are ghosts in feed_tru
+        #no_service_routes = {}
+        # effect 1 is NO_SERVICE
+        #for x in [x for x in alerts.entity if x.alert.effect == 1]:
+        #    r_id = list(set([ie.route_id for ie in x.alert.informed_entity]))
+        #    no_service_routes.update({r:[x.alert.active_period[0].start, x.alert.active_period[0].end] for r in r_id})
+
+        #filter out trip_update feed from cancelled and not yet departed routes
+        #no_service_routes.keys() and
+        url_trip_upd = "{}/TripUpdates.pb?key={}".format(base_link, key)
+
+        feed_tru = gtfs_realtime_pb2.FeedMessage()
+
+        r_tru = requests.get(url_trip_upd)
+        if r_tru.status_code != 200:
+            raise AirflowException('could not get alerts data')
+
+        feed_tru.ParseFromString(r_tru.content)
+
+        tru_filtered = [x for x in feed_tru.entity \
+            if not x.trip_update.trip.route_id in alerts and \
+               x.trip_update.trip.schedule_relationship != 3 and \
+               x.trip_update.stop_time_update[0].arrival.time <= feed_tru.header.timestamp]
+
+        #contains all the data which is going to the db
+        vhc_array = []
+        for x in tru_filtered:
+            if x.trip_update.vehicle.id == '':
+                continue
+            tu = x.trip_update
+            tut = tu.trip
+            data = {'meta':{'date':tut.start_date, 'trip_id':tut.trip_id, 'route_id':tut.route_id, \
+                            'vehicle_id':tu.vehicle.id, 'id': x.id},
+                    'next_stop_index':0, 'stop_num':len(tu.stop_time_update), 'alive':True,
+                    'stops':[]}
+            timestamp = tu.timestamp
+            for si, s in enumerate(tu.stop_time_update):
+                stop_dic = {'stop_id':s.stop_id, 'alert_id':None,
+                            'arrival_timestamp':0, 'departure_timestamp':0,
+                            'congestion_level':0, 'current_status':0, 'top_speed':0}
+                if s.arrival.time <= timestamp:
+                    stop_dic['arrival_timestamp'] = s.arrival.time
+                    stop_dic['delay'] = s.arrival.delay
+                    data['next_stop_index'] = si + 1
+                if s.departure.time <= timestamp:
+                    stop_dic['departure_timestamp'] = s.departure.time
+                data['stops'].append(stop_dic)
+            if data['next_stop_index'] == data['stop_num']:
+                data['alive'] = False
+            vhc_array.append(data)
+
+        return vhc_array
+
+    @task()
+    def load(processed_input):
+        server = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000"
+        db_name = 'bkk_data'
+        col_vehicle = 'vehicle_trip'
+
+        try:
+            client = MongoClient(server)
+            client.server_info()
+        except:
+            raise AirflowException('Couldn\'t estabilish connection with MongoDB server.')
+
+        db = client[db_name]
+        # get the trip_id-date pairs from the db and filter what's only needed to be updated
+        # FIXME are trip_ids unique to a day?
+        trip_id_curs = db[col_vehicle].find({'meta.date':date.today().strftime('%Y%m%d')})
+        todays_trips = [x['meta']['trip_id'] for x in trip_id_curs]
+
+        vhc_new = []
+
+        if len(todays_trips) == 0:
+            vhc_new = processed_input
+        else:
+            for x in processed_input:
+                if not x['meta']['trip_id'] in todays_trips:
+                    vhc_new.append(x)
+                    continue
+                db_id_filter = {'meta.id':x['meta']['id']}
+                db_alive = db[col_vehicle].find_one(db_id_filter, {'alive':1, '_id':0})
+                if db_alive:
+                    db[col_vehicle].update_one(db_id_filter, \
+                                               {'$set':{'next_stop_index':x['next_stop_index']}})
+                    db_next_stop = db[col_vehicle].find_one(db_id_filter,
+                                                        {'next_stop_index':1, '_id':0})['next_stop_index']
+                    for i in range(db_next_stop, x['next_stop_index']):
+                        stop = x['stops'][i]
+                        if stop['arrival_timestamp'] and stop['departure_timestamp'] == 0:
+                            break
+                        if stop['arrival_timestamp'] != 0:
+                            db[col_vehicle].update_one(db_id_filter, \
+                                {'$set':{'stops.'+str(i)+'.arrival_timestamp':stop['arrival_timestamp']}})
+                        if stop['departure_timestamp'] != 0:
+                            db[col_vehicle].update_one(db_id_filter, \
+                                {'$set':{'stops.'+str(i)+'.departure_timestamp':stop['departure_timestamp']}})
+                    if x['alive'] == False:
+                        db[col_vehicle].update_one(db_id_filter, {'$set':{'alive':False}})
+
+        db[col_vehicle].insert_many(vhc_new)
+        client.close()
+
+    #data = extract()
+    alerts = get_current_alerts()
+    processed_data = transform(0, alerts)
+    load(processed_data)
+
+bkk_trip()
